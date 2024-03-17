b2e7b011286e5c877aa7589771e6c69ec256c5b4
lgh <573363068@qq.com>
2024-03-17 17:46:11 +0800

    Revert "test1"
    This reverts commit febf1f8b2673848e1dda7a3cb8ccbdf2ed00662d.

--- src/views/center/Center.vue
+++ src/views/center/Center.vue
@@ -92,7 +92,7 @@ const handleChange = file => {
     userForm.avatar = URL.createObjectURL(file);
     userForm.file = file;
 }
-//22222222222
+
 //更新提交
 const submitForm = () => {
     userFormRef.value.validate(async (valid) => {
