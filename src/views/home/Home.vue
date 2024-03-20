1940ef011b1ff1850769b95b3b51aac3e5cae44b
lgh <573363068@qq.com>
2024-03-20 16:28:58 +0800

    admin首页产品展示--已完成

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -17,33 +17,62 @@
                     <span>公司产品</span>
                 </div>
             </template>
-            <el-carousel :interval="4000" type="card" height="200px">
-                <el-carousel-item v-for="item in 6" :key="item">
-                    <h3 text="2xl" justify="center">{{ item }}</h3>
+            <el-carousel v-if="loopList.length" :interval="4000" type="card" height="200px">
+                <el-carousel-item v-for="item in  loopList " :key="item._id">
+                    <div :style="{ backgroundImage: `url(http://localhost:3000${item.cover})`,backgroundSize:'cover'}">
+                   <!-- <h3> {{ item.title }}</h3> -->
+                    </div>
                 </el-carousel-item>
-            </el-carousel>
+            </el-carousel> 
         </el-card>
     </div>
 </template>
 <script setup>
 import { useStore } from "vuex"
-import { computed } from "vue"
-const store = useStore()
+import { computed, onMounted, ref } from "vue"
+import axios from "axios";
 
+
+const store = useStore()
+const loopList = ref([])
 const avatarUrl = computed(() => store.state.userInfo.avatar ? 'http://localhost:3000' + store.state.userInfo.avatar : `https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png`)
-const welcomeText = computed(() => new Date().getHours() < 12 ? '开开心心每一天！' : '喝杯咖啡提提神吧！')
+const welcomeText = computed(() => {
+    const hour = new Date().getHours();
+    if (hour >= 6 && hour < 12) {
+        return '上午好！';
+    } else if (hour >= 12 && hour < 18) {
+        return '下午好！';
+    } else {
+        return '晚上好！';
+    }
+});
+
+onMounted(() => {
+    getData()
+})
+
+const getData = async () => {
+    const res = await axios.get(`/adminapi/product/list`)
+    // console.log(res.data);
+    loopList.value = res.data.data
+    console.log(loopList.value);
+}
+
 </script> 
 <style lang="scss" scoped>
 .box-card {
     margin-top: 50px;
 }
 
-.el-carousel__item h3 {
-    color: #475669;
-    opacity: 0.75;
-    line-height: 200px;
-    margin: 0;
-    text-align: center;
+// .el-carousel__item h3 {
+//     color: #010914f7;
+//     opacity: 0.75;
+//     line-height: 200px;
+//     margin: 0;
+//     text-align: center;
+// }
+.el-carousel__item div {
+    height: 100%;
 }
 
 .el-carousel__item:nth-child(2n) {
